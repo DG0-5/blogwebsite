a1e83473c726f0cdfd52cb564a9467608a9a2bd9
dg0-5 <dg0.5@outlook.com>
2024-04-04 19:57:25 -0500

    Final Project

--- users/models.py
+++ users/models.py
@@ -9,9 +9,11 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
         img = Image.open(self.image.path)
+        
         if img.height > 1024 or img.width > 1024:
             output_size = (1024, 1024)
             img.thumbnail(output_size)
